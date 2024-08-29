9db84fa7f39b5fc7435456267789c4283f445f8b
Jennifer Okeke <aestheticjenny04@gmail.com>
2024-08-29 18:08:43 +0100

    create function getComputerChoice

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # RockPaperScissors
+
+Exercise from the Odin Project
+
+PseudoCode
+Collect Player's choice
+Collect Computer random choice
+Compare Player chioce and Computer choice
+If Player 1's choice is equal to Computer's chioce then it's a draw
+If player 1 chooses Rock AND Player 2 chooses Scissors THEN Player 1 wins!
+IF Player 1 chooses Scissors AND Player 2 chooses Paper THEN Player 1 wins!
+IF Player 1 chooses Paper AND Player 2 chooses Rock THEN Player 1 wins!
+Else Player 2 wins!
+
---
+++ main.js
@@ -0,0 +1,24 @@
+// Collect Player's choice
+// player = prompt("rock, paper, scissors: ")
+// Collect Computer random choice
+// Compare Player chioce and Computer choice
+// If Player 1's choice is equal to Computer's chioce then it's a draw
+// If player 1 chooses Rock AND Player 2 chooses Scissors THEN Player 1 wins!
+// IF Player 1 chooses Scissors AND Player 2 chooses Paper THEN Player 1 wins!
+// IF Player 1 chooses Paper AND Player 2 chooses Rock THEN Player 1 wins!
+// Else Player 2 wins!
+
+//Create a new function named getComputerChoice 
+
+function getComputerChoice() {
+    let randomNum = Math.floor(Math.random() * 3) + 1;
+    if (randomNum === 1) {
+        return "rock";
+    } else if (randomNum === 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
